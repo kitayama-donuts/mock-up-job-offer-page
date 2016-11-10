54b8a4b02405cdb166e569e7772545bec72790af
kitayama-donuts <kitayama.minoru@donuts.ne.jp>
2016-11-10 10:26:00 +0900

    求人詳細まで

--- static/js/search.js
+++ static/js/search.js
@@ -1,3 +1,41 @@
 $('.js-toggle-search').on('click', function(){
   $('.sp-search-content').toggleClass('is-hide');
 });
+
+var swiper = new Swiper('.swiper-container', {
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    }
+  }
+});
+
+// var swiper = new Swiper('.swiper-container', {
+//   nextButton: '.swiper-button-next',
+//   prevButton: '.swiper-button-prev',
+//   pagination: '.swiper-pagination',
+//   paginationClickable: true,
+//   // Disable preloading of all images
+//   preloadImages: false,
+//   // Enable lazy loading
+//   lazyLoading: true
+// });
